ecf2263381f0a2dbcbf7c985d38cf92e33ee5370
Jorge Acosta <canelitajr13@gmail.com>
2022-03-22 08:51:33 -0600

    Fisrt commit

---
+++ src/uaslp/objetos/parcial2/exam/exporters/ExporterFactory.java
@@ -0,0 +1,13 @@
+package uaslp.objetos.parcial2.exam.exporters;
+
+public class ExporterFactory implements Exporter{
+
+    public static Exporter getExporter(ExporterType json) {
+        return null;
+    }
+
+    @Override
+    public String export(String message) {
+        return null;
+    }
+}
---
+++ src/uaslp/objetos/parcial2/exam/messagebuilder/MessageBuilderFactory.java
@@ -0,0 +1,8 @@
+package uaslp.objetos.parcial2.exam.messagebuilder;
+
+public class MessageBuilderFactory {
+
+    public static MessageBuilder getMessageBuilder(MessageType messageType) {
+        return null;
+    }
+}
